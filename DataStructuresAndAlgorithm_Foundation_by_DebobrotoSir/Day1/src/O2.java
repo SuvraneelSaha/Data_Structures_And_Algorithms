ae040c5571264be9449d6739f95d2056f8926c6b
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-06-13 13:48:18 +0530

    starting

---
+++ DataStructuresAndAlgorithm_Foundation_by_DebobrotoSir/Day1/src/O2.java
@@ -0,0 +1,44 @@
+public class O2 {
+    public static void main(String[] args) {
+/*
+Dont waste time - time is precious
+dont sit idle
+Sleeping and talking to a friend is much worthy
+
+ */
+        System.out.println("Starting Programming ");
+
+/*
+
+IBM Watson - search
+ai something
+
+Autopilot mode - search
+Machine drives the flight
+
+
+Japan - driver less cars
+
+Online games
+
+computer Chess Games
+
+we think that a machine is playing the game
+but in reality it is just well wriiten code and it is soo
+well written that we can differentiate that it is a person or not ie real or not
+
+
+Sophia - search
+
+
+2 chatbots start talking and invented their own language
+
+Face book forcefully stopped
+
+
+ */
+
+
+
+    }
+}
---
+++ DataStructuresAndAlgorithm_Foundation_by_DebobrotoSir/Day1/src/O3.java
@@ -0,0 +1,10 @@
+public class O3 {
+    public static void main(String[] args) {
+        /*
+            6 feet into cm
+
+         */
+
+
+    }
+}
