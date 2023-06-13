84633b26518637652e77f4e4589891b604e153f5
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-06-13 11:47:45 +0530

    the start is the end the end is the start

---
+++ DataStructuresAndAlgorithm_Foundation_by_DebobrotoSir/Day1/src/O1.java
@@ -0,0 +1,13 @@
+public class O1 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("You can get anything you put your mind into");
+        System.out.println("2");
+
+        for (int i = 0; i < 5; i++)
+        {
+           i=i+2;
+            System.out.println(i);
+        }
+    }
+}
