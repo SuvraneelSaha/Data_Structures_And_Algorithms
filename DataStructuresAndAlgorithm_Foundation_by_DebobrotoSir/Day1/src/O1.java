d294f3fe831fb27449f83488f332c0ecd588ab15
Suvraneel _Saha <suvroneel2001@gmail.com>
2023-08-04 20:30:21 +0530

    starting afresh

--- DataStructuresAndAlgorithm_Foundation_by_DebobrotoSir/Day1/src/O1.java
+++ DataStructuresAndAlgorithm_Foundation_by_DebobrotoSir/Day1/src/O1.java
@@ -2,8 +2,17 @@ public class O1 {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("You can get anything you put your mind into");
-        System.out.println("2");
+        System.out.println("Condition ; consistency == success ");
+        System.out.print("2"); // point of example
 
+        System.out.println();
+/*
+If we use any ln statement like println then after printing the cursor or line cursor will go to
+the next line
+otherwise not
+it will be in the same line only , it will stay in the same line
+
+ */
         for (int i = 0; i < 5; i++)
         {
            i=i+2;
